c7f8a5cd5aa8972a7f100de4ece11f29b6066bfa
Nikita Vdovichev <91666483+vdovichevnick@users.noreply.github.com>
2024-05-10 19:25:11 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 5174 // Укажите порт, на котором вы хотите запустить ваше приложение
+  }
+});
